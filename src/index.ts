f068fcca3db797c9f1c7d533854687de013a4d49
Sandro Soria <sandro33810@gmail.com>
2025-01-24 16:11:21 +0100

    feat: add readline interface for graceful server shutdown

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
-// filepath: src/index.ts
 import express from 'express';
+import readline from 'readline';
 
 const app = express();
 const port = 8443;
@@ -8,6 +8,24 @@ app.get('/', (req, res) => {
   res.send('Hello, world!');
 });
 
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`Server is running on https://localhost:${port}`);
+});
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.question('Type "yes" to disconnect: ', (answer) => {
+  if (answer.toLowerCase() === 'yes') {
+    console.log('Disconnecting...');
+    server.close(() => {
+      console.log('Server disconnected');
+      process.exit(0);
+    });
+  } else {
+    console.log('Server continues to run');
+  }
+  rl.close();
 });
